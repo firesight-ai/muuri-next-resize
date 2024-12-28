2de031d02e52786f585275df34844332af3f0f9e
bitnom <14287229+bitnom@users.noreply.github.com>
2024-12-28 09:52:57 -0500

    rm bak/

--- bak/utils/items.js
+++
@@ -1,9 +0,0 @@
-const types = ["Account", "Monthly Budget", "Goals"];
-
-export const generateItems = (amount = 9) =>
-  Array(amount)
-    .fill()
-    .map((_, id) => ({
-      id,
-      type: types[id % types.length]
-    }));
